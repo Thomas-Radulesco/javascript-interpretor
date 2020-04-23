c88dcd1c29b3e6115a99aa050910a5d3d155df45
Thomas-Radulesco <tom.radulesco@gmail.com>
2020-04-23 18:47:07 +0200

    board + currentPosition ok

--- js/app.js
+++ js/app.js
@@ -7,6 +7,36 @@ var app = {
 
     // Event listeners - TODO
   },
+
+  drawBoard: function() {
+    let currentPosition = [1,1]
+    console.log(currentPosition)
+    for(let i=1;i<=4;i++){
+      let newRow = document.createElement('div')
+      newRow.classList.add('cellRow')
+      newRow.setAttribute('id', 'row'+i)
+      for(let j=1;j<=6;j++){
+        let newCell = document.createElement('div')
+        newCell.classList.add('cell')
+          if((i==1)&&(j==1)){
+            newCell.classList.add('cellStart')
+          }
+          if((i==4)&&(j==6)){
+            newCell.classList.add('cellEnd')
+          }
+          if((i==currentPosition[1])&&(j==currentPosition[0])){
+            newCell.classList.add('cellCurrent')
+          }
+
+        newRow.appendChild(newCell)
+      }
+     
+      document.getElementById('board').appendChild(newRow)
+    }
+
+
+
+  },
   handleLaunchScriptButton: function() {
     // TODO
     
