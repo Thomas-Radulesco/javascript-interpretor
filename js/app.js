5e071f4eb30d0e706463633b8274585db05b023c
Thomas-Radulesco <tom.radulesco@gmail.com>
2020-04-25 00:31:58 +0200

    interpreteur ok

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var app = {
-  x:1,
-  y:1,
-  direction:'right',
-  init: function() {
+  x: 1,
+  y: 1,
+  direction: 'right',
+  init: function () {
     console.log('init');
 
     // TODO
@@ -15,140 +15,142 @@ var app = {
 
     // Event listeners - TODO
     document.addEventListener('keydown', app.handleKeys)
+    document.getElementById('launchScript').addEventListener('click', app.handleLaunchScriptButton)
 
   },
-  handleKeys: function(evt) {
-    if(evt.key==='ArrowUp'){
+  handleKeys: function (evt) {
+    if (evt.key === 'ArrowUp') {
       app.moveForward()
       // app.eraseBoard()
       app.drawBoard()
     }
-    if(evt.key==='ArrowRight'){
+    if (evt.key === 'ArrowRight') {
       app.turnRight()
       // app.eraseBoard()
       app.drawBoard()
     }
-    if(evt.key==='ArrowLeft'){
+    if (evt.key === 'ArrowLeft') {
       app.turnLeft()
       // app.eraseBoard()
       app.drawBoard()
     }
   },
-  moveForward: function() {
+  moveForward: function () {
     // si app.direction == right alors app.x++
-    if(app.direction=='right'){
+    if (app.direction == 'right') {
       app.x++
     }
     // si app.direction == top alors app.y--
-    if(app.direction=='top'){
+    if (app.direction == 'top') {
       app.y--
     }
     // si app.direction == left alors app.x--
-    if(app.direction=='left'){
+    if (app.direction == 'left') {
       app.x--
     }
     // si app.direction == bottom alors app.y++
-    if(app.direction=='bottom'){
+    if (app.direction == 'bottom') {
       app.y++
     }
-    if((app.x<1)||
-        (app.x>6)||
-        (app.y<1)||
-        (app.y>4)){
-          alert('Vous êtes sorti du cadre - GAME OVER')
-          app.x = 1
-          app.y = 1
-          app.direction = 'right'
-          app.init()
-        }
+    if ((app.x < 1) ||
+      (app.x > 6) ||
+      (app.y < 1) ||
+      (app.y > 4)) {
+      alert('Vous êtes sorti du cadre - GAME OVER')
+      app.x = 1
+      app.y = 1
+      app.direction = 'right'
+      app.init()
+    }
   },
 
-  turnRight: function() {
+  turnRight: function () {
     // si app.direction == right alors app.direction = bottom
-    if(app.direction=='right'){
+    if (app.direction == 'right') {
       app.direction = 'bottom'
       return
     }
     // si app.direction == top alors app.direction = right
-    if(app.direction=='top'){
+    if (app.direction == 'top') {
       app.direction = 'right'
       return
     }
     // si app.direction == left alors app.direction = top
-    if(app.direction=='left'){
+    if (app.direction == 'left') {
       app.direction = 'top'
       return
     }
     // si app.direction == bottom alors app.direction = left
-    if(app.direction=='bottom'){
+    if (app.direction == 'bottom') {
       app.direction = 'left'
       return
     }
   },
 
-  turnLeft: function() {
+  turnLeft: function () {
     // si app.direction == right alors app.direction = top
-    if(app.direction=='right'){
+    if (app.direction == 'right') {
       app.direction = 'top'
       return
     }
     // si app.direction == top alors app.direction = left
-    if(app.direction=='top'){
+    if (app.direction == 'top') {
       app.direction = 'left'
       return
     }
     // si app.direction == left alors app.direction = bottom
-    if(app.direction=='left'){
+    if (app.direction == 'left') {
       app.direction = 'bottom'
       return
     }
     // si app.direction == bottom alors app.direction = right
-    if(app.direction=='bottom'){
+    if (app.direction == 'bottom') {
       app.direction = 'right'
       return
     }
   },
 
-  drawBoard: function() {
-    document.getElementById('board').innerHTML=""
+  drawBoard: function () {
+    document.getElementById('board').innerHTML = ""
     let currentPosition = [app.x, app.y]
     let currentDirection = app.direction
     console.log(currentPosition)
-    for(let i=1;i<=4;i++){
+    for (let i = 1; i <= 4; i++) {
+      // i correspond à app.y & j correspond à app.x
       let newRow = document.createElement('div')
       newRow.classList.add('cellRow')
-      newRow.setAttribute('id', 'row'+i)
-      for(let j=1;j<=6;j++){
+      newRow.setAttribute('id', 'row' + i)
+      for (let j = 1; j <= 6; j++) {
         let newCell = document.createElement('div')
         newCell.classList.add('cell')
-          if((i==1)&&(j==1)){
-            newCell.classList.add('cellStart')
-          }
-          if((i==4)&&(j==6)){
-            newCell.classList.add('cellEnd')
-          }
-          if((i==currentPosition[1])&&(j==currentPosition[0])){
-            newCell.classList.add('cellCurrent')
-          }
-            if(currentDirection=='right'){
-            newCell.classList.add('cellCurrent-right')
-          }
-          if(currentDirection=='top'){
-            newCell.classList.add('cellCurrent-top')
-          }
-          if(currentDirection=='left'){
-            newCell.classList.add('cellCurrent-left')
-          }
-          if(currentDirection=='bottom'){
-            newCell.classList.add('cellCurrent-bottom')
-          }
+        if ((i == 1) && (j == 1)) {
+          newCell.classList.add('cellStart')
+        }
+        if ((i == 4) && (j == 6)) {
+          newCell.classList.add('cellEnd')
+        }
+        if ((i == currentPosition[1]) && (j == currentPosition[0])) {
+          newCell.classList.add('cellCurrent')
+        }
+        if (currentDirection == 'right') {
+          newCell.classList.add('cellCurrent-right')
+        }
+        if (currentDirection == 'top') {
+          newCell.classList.add('cellCurrent-top')
+        }
+        if (currentDirection == 'left') {
+          newCell.classList.add('cellCurrent-left')
+        }
+        if (currentDirection == 'bottom') {
+          newCell.classList.add('cellCurrent-bottom')
+        }
 
-          
+
 
 
         newRow.appendChild(newCell)
       }
-     
+
       document.getElementById('board').appendChild(newRow)
     }
 
@@ -156,40 +158,66 @@ var app = {
 
   },
 
-  handleLaunchScriptButton: function() {
+  handleLaunchScriptButton: function () {
     // TODO
-    
     // TODO : get all lines as an array
-
-    window.setTimeout(function() {
-      app.codeLineLoop(codeLines, 0);
-    }, 2000);
+    let codeLines = document.getElementById('userCode').value.split('\n')
+    // console.log(codeLines)
+    // Appel, après 2sec, la méthode codeLineLoop() avec la variable et l'index de départ (0) 2000 au lieu de 100
+    window.setTimeout(function () {
+      app.codeLineLoop(codeLines, 0)
+    }, 100)
   },
 
-  codeLineLoop: function(codeLines, index) {
+  codeLineLoop: function (codeLines, index) {
     // Getting currentLine
-    var currentLine = codeLines[index];
-    console.log(currentLine);
+    var currentLine = codeLines[index]
+    console.log(currentLine)
+    // console.log(typeof currentLine)
+    // eval() fonctionne mais pas safe ! ne pas utiliser
+    // eval(currentLine)
 
+    moves = ['moveForward', 'turnRight', 'turnLeft']
+
+    if (moves.indexOf(currentLine) < 0) {
+      alert('Merci d\'entrer des fonctions valides parmi : moveForward, turnRight ou turnLeft')
+      return
+    } else {
+      // Function fonctionne, mais erreur en console si input vide ... sécurité ???
+      Function('"use strict";return (app.' + currentLine + '())')()
+      app.drawBoard()
+    }
+    // Test de sécurité, à c/c dans l'input; si ça s'exécute, c'est pas bon !
+    // window.location = 'http://www.google.com'
 
     // Increment
     index++;
 
     // if still a line to interpret
     if (index < codeLines.length) {
-      // Recall same method (=> make a loop)
-      window.setTimeout(function() {
-        app.codeLineLoop(codeLines, index);
-      }, 1000);
+      // Recall same method (=> make a loop) 1000 au lieu de 100
+      window.setTimeout(function () {
+        app.codeLineLoop(codeLines, index)
+      }, 100)
     } else {
-      window.setTimeout(function() {
-        app.checkSuccess();
-      }, 1000);
+      window.setTimeout(function () {
+        app.checkSuccess()
+      }, 100)
     }
   },
 
-  checkSuccess: function() {
+  checkSuccess: function () {
     // TODO display if the game is won or not
+    if (app.x == 6 && app.y == 4) {
+      alert('CONGRATULATIONS, YOU WIN !')
+      return
+    }
+    alert('SORRY, YOU LOOSE ! TRY AGAIN !')
+    app.x = 1
+    app.y = 1
+    app.direction = 'right'
+    app.init()
+    return
   }
 };
 
